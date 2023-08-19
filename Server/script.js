c4122ad957cc9c4ceb7a0b29bf1e8fb809e58709
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-08-19 11:26:07 +0700

    Update script.js

--- Server/script.js
+++ Server/script.js
@@ -1,14 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = [
-        { id: "japanproject", url: "https://github.com/pooooooon/JavaPlayer" },
-        { id: "bluemyth", url: "https://github.com/BlueMyth-Network" },
-        { id: "cmgmmo", url: "../minecraft/" },
         { id: "cmgmmodis", url: "https://discord.com/invite/JjchmEtqXp" },
-        { id: "project", url: "./ProJect/" },
-        { id: "connect", url: "https://lnwaitji.github.io/Minecraft/CMGMMO?ontab=true" },
-        { id: "back", url: "https://lnwAitJi.github.io?ontab=true" },
-        { id: "r", url: "https://lnwAitJi.github.io/database?ontab=true" },
-        { id: "server", url: "./Server/" }
     ];
 
     buttons.forEach(button => {
