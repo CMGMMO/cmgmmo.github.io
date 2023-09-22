69cd91d64926b58d7c70afc3b0e917e2a1a81685
Inw AitJi <jariya232560@gmail.com>
2023-09-22 17:13:14 +0700

    join tournament pls

---
+++ Tournament/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = [
+        { id: "cmgmmodis", url: "https://discord.com/invite/JjchmEtqXp" },
+        { id: "jointournament", url: "https://docs.google.com/forms/d/e/1FAIpQLSfryGqwfEjCUhTqdG4iZ0nVh9vcVwg2VGXgPaGDqY-qrjgDUw/viewform" }
+    ];
+
+    buttons.forEach(button => {
+        const element = document.getElementById(button.id);
+        if (element) {
+            element.addEventListener("click", () => {
+                if(button.url.endsWith("?ontab=true")){
+                  const herfTo = button.url.split("?onTab=true")[0] ?? button.url
+                  window.location.href = herfTo;
+                  return
+                }
+                if (button.url.startsWith("http")) window.open(button.url, "_blank");
+                else window.location.href = button.url;
+                
+            });
+        } else {
+            console.error(`Element with ID '${button.id}' not found.`);
+        }
+    });
+});
