c46fa452d0c75c515beacf1faf5f87e579cbd53b
Inw AitJi <jariya232560@gmail.com>
2023-09-23 08:50:49 +0700

    oftimize?

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = [
+        { id: "cmgmmodis", url: "../discord/" },
+        { id: "plus", url: "../plus/"},
+        { id: "tour", url: "../Tournament/"},
         { id: "1.2.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.2.0/CMGMMO-Plus-beta.1.2.0.mcpack" },
         { id: "1.1.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.1.0/CMGMMO-Plus-beta.1.1.0.mcpack" },
         { id: "1.0.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.0.0/CMGMMO-Plus-beta.1.0.0.mcpack" },
-        { id: "cmgmmodis", url: "../discord/" },
         { id: "github", url: "https://github.com/lnwAitJi/CMGMMO-Plus/" },
         { id: "back", url: "../Server/" },
+        { id: "jointournament", url: "https://docs.google.com/forms/d/e/1FAIpQLSfryGqwfEjCUhTqdG4iZ0nVh9vcVwg2VGXgPaGDqY-qrjgDUw/viewform?ontab=true" }
     ];
 
     buttons.forEach(button => {
