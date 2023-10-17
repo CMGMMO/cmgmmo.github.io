21150450ce001e3738cd329e956cec4e51191b41
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-10-17 20:12:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
             { id: "cmgmmodiscord", url: "https://discord.com/invite/JjchmEtqXp" },
             { id: "plus", url: "../plus/" },
             { id: "tour", url: "../Tournament/" },
-            { id: "1.3.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.3.0/CMGMMO-Plus-beta.1.3.0.mcpack" },
-            { id: "1.2.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.2.0/CMGMMO-Plus-beta.1.2.0.mcpack" },
-            { id: "1.1.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.1.0/CMGMMO-Plus-beta.1.1.0.mcpack" },
-            { id: "1.0.0", url: "https://github.com/lnwAitJi/CMGMMO-Plus/releases/download/beta1.0.0/CMGMMO-Plus-beta.1.0.0.mcpack" },
             { id: "github", url: "https://github.com/lnwAitJi/CMGMMO-Plus/" },
             { id: "back", url: "../Server/" },
             { id: "jointournament", url: "https://docs.google.com/forms/d/e/1FAIpQLSfryGqwfEjCUhTqdG4iZ0nVh9vcVwg2VGXgPaGDqY-qrjgDUw/viewform?ontab=true" }
